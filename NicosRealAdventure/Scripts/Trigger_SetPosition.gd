53559540c9f82079488586315c2f647c8b554369
Pope Scooby <PopeScooby@gmail.com>
2023-02-05 17:50:47 -0600

    Teleporter working

---
+++ NicosRealAdventure/Scripts/Trigger_SetPosition.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+export var move_x = 0.00
+export var move_y = 0.00
+export var body_list = ["Crate"]
+
+func _ready():
+	pass # Replace with function body.
+
+
+#func _process(delta):
+#	pass
+
+
+func _on_SetPosition_body_entered(body):
+	if body.name.find("Crate") != -1:
+		var curr_pos = body.position
+		var new_pos = curr_pos + Vector2(move_x, move_y)
+		body.position = new_pos
+
+func _on_SetPosition_body_exited(body):
+	pass # Replace with function body.
